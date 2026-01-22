ad610c5fa8713b24e06ae8a2dae41adecefc0f48
JCGuerrero <JCystems@gMail.com>
2026-01-22 13:00:25 -0600

    wa

--- src/agent/config/os_environ/azure_openai.py
+++ src/agent/config/os_environ/azure_openai.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel, Field
 
 
 class AzureOpenAISettings(BaseModel):
-    endpoint: str = Field()  # min_length=10)
-    deployment: str = Field()  # min_length=2)
-    api_version: str = Field()  # min_length=2)
+    endpoint: str = Field(min_length=10)
+
+    deployment: str = Field(min_length=2)
+
+    api_version: str = Field(min_length=2)
